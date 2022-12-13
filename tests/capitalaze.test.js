4020106e7e91566d44a47882d11d0cdc138a128b
Сергей Толстиков <tolstikovsn@gmail.com>
2022-12-13 18:29:38 +0500

    test update with "assert"

--- tests/capitalaze.test.js
+++ tests/capitalaze.test.js
@@ -1,20 +1,23 @@
-// нажмите кнопку run чтобы запустить тесты
-// попробуйте изменять код функции / тестов, запуская проверки заново
 
 import { capitalize } from '../src/capitalaze.js';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
-if (capitalize('how are you') !== 'How are you') {
-    throw new Error('Функция работает неверно!');
-  }
-if (capitalize('pivo') !== 'pivo') {
-    throw new Error('Проверка ошибки в тесте!');
-  }
+import { strict as assert } from 'node:assert';
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+// // if (capitalize('hello') !== 'Hello') {
+// //   throw new Error('Функция работает неверно!');
+// // }
+// if (capitalize('how are you') !== 'How are you') {
+//     throw new Error('Функция работает неверно!');
+//   }
+// if (capitalize('pivo') !== 'pivo') {
+//     throw new Error('Проверка ошибки в тесте!');
+//   }
+// if (capitalize('') !== '') {
+//   throw new Error('Функция работает неверно!');
+// }
+// console.log('Все тесты успешно пройдены!');
 
-console.log('Все тесты успешно пройдены!');
+assert.deepEqual(capitalize(''), '');
+assert.deepEqual(capitalize('hello'),'Hello');
+assert.deepEqual(capitalize('how are you'),'How are you');
+assert.deepEqual(capitalize('pivo'), 'Pivo');
